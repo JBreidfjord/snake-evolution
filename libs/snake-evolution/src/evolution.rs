c6351c3791fd89b7f27ca9439cfb7f7d80bd0955
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-06-04 09:46:05 -0600

    define step and train fns

--- libs/snake-evolution/src/evolution.rs
+++ libs/snake-evolution/src/evolution.rs
@@ -15,6 +15,7 @@ const POPULATION_SIZE: usize = 2000;
 const GAME_GRID_SIZE: usize = 10;
 const MUTATION_RATE: f32 = 0.15;
 const MUTATION_STRENGTH: f32 = 0.3;
+const GENERATION_LENGTH: usize = 10_000;
 
 pub(crate) struct Evolution {
     population: Vec<Snake>,
@@ -37,6 +38,38 @@ impl Evolution {
             population,
             ga,
             age: 0,
+        }
+    }
+
+    pub(crate) fn step(&mut self, rng: &mut dyn RngCore) {
+        self.process_brains();
+        self.process_movement();
+        self.process_finished_games();
+        self.process_evolution(rng);
+
+        self.age += 1;
+    }
+
+    fn process_brains(&mut self) {
+        todo!("Implement process brains")
+    }
+
+    fn process_movement(&mut self) {
+        todo!("Implement process movement")
+    }
+
+    fn process_finished_games(&mut self) {
+        todo!("Implement process finished games")
+    }
+
+    fn process_evolution(&mut self, rng: &mut dyn RngCore) {
+        todo!("Implement process evolution")
+    }
+
+    /// Step until end of current generation
+    pub(crate) fn train(&mut self, rng: &mut dyn RngCore) {
+        while self.age < GENERATION_LENGTH {
+            self.step(rng);
         }
     }
 }
