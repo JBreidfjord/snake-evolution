19f634ba8d035536fd42c801d91c89d3d3c032fb
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-06-04 00:04:15 -0600

    add test for direction value

--- libs/snake-evolution/src/snake/direction.rs
+++ libs/snake-evolution/src/snake/direction.rs
@@ -16,3 +16,16 @@ impl Direction {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_value() {
+        assert_eq!(Direction::Up.value(), (0, -1));
+        assert_eq!(Direction::Down.value(), (0, 1));
+        assert_eq!(Direction::Left.value(), (-1, 0));
+        assert_eq!(Direction::Right.value(), (1, 0));
+    }
+}
