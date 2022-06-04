43bebba577cafac2287b93ec422fe83094e6e9f4
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-06-03 23:56:29 -0600

    define game loop

--- libs/snake-evolution/src/main.rs
+++ libs/snake-evolution/src/main.rs
@@ -1,3 +1,30 @@
+use console::Term;
+
+use snake::direction::Direction;
+use snake::game::Game;
+
+mod snake;
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = Term::buffered_stdout();
+    let mut game = Game::new(10);
+
+    'game_loop: loop {
+        stdout.clear_screen().unwrap_or_default();
+        println!("{}", game.display());
+
+        let direction = if let Ok(character) = stdout.read_char() {
+            match character {
+                'w' => Direction::Up,
+                'a' => Direction::Left,
+                's' => Direction::Down,
+                'd' => Direction::Right,
+                _ => break 'game_loop,
+            }
+        } else {
+            panic!("Failed to read input")
+        };
+
+        game.move_snake(direction);
+    }
 }
