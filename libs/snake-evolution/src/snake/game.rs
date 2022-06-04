a6b0196be79c3ae09ff24442074902491b2a73e6
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-06-04 00:01:09 -0600

    add tests for new and move fns

--- libs/snake-evolution/src/snake/game.rs
+++ libs/snake-evolution/src/snake/game.rs
@@ -33,5 +33,53 @@ impl Game {
 
 }
 
-}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod new {
+        use super::*;
+
+        #[test]
+        fn test_initial_snake_placement() {
+            let game = Game::new(10);
+            assert_eq!(game.snake, VecDeque::from([44]));
+
+            let game = Game::new(3);
+            assert_eq!(game.snake, VecDeque::from([4]));
+        }
+
+        #[test]
+        fn test_initial_food_placement() {
+            let game = Game::new(3);
+            assert_ne!(game.food, 4);
+            assert!((0..9).contains(&game.food));
+
+            let game = Game::new(10);
+            assert_ne!(game.food, 44);
+            assert!((0..100).contains(&game.food));
+        }
+    }
+
+    mod move_snake {
+        use super::*;
+
+        #[test]
+        fn test_move() {
+            let mut game = Game::new(3);
+            game.move_snake(Direction::Left);
+            game.food = 0;
+            assert_eq!(game.snake, VecDeque::from([4, 3]));
+
+            game.move_snake(Direction::Down);
+            assert_eq!(game.snake, VecDeque::from([3, 6]));
+
+            game.move_snake(Direction::Right);
+            assert_eq!(game.snake, VecDeque::from([6, 7]));
+
+            game.move_snake(Direction::Up);
+            assert_eq!(game.snake, VecDeque::from([7, 4]));
+        }
+    }
 }
